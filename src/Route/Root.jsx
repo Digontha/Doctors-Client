f1fc6896ad383f55da21895f2bb34df82d22e879
Digontha <digonthodj@gmail.com>
2023-12-14 15:35:54 +0600

    create fotter and implement

--- src/Route/Root.jsx
+++ src/Route/Root.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Shared/Navbar';
+import Fotter from '../Shared/Fotter';
 
 const Root = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Fotter></Fotter>
         </div>
     );
 };
