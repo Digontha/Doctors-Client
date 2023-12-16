ff1aaa0e83393cc8baf73850d9ab2453aad1d0d1
Digontha <digonthodj@gmail.com>
2023-12-16 19:50:21 +0600

    get data of doctors route in find doctors page

--- src/Pages/FindDoctor.jsx
+++ src/Pages/FindDoctor.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import useDoctors from '../Hook/useDoctors';
 
 const FindDoctor = () => {
+    const [Doctors]=useDoctors()
+    console.log(Doctors);
     return (
         <div>
             FindDoctor page
